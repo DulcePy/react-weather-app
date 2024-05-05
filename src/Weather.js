2c6a59f1fed38142b8bd453e57e7405c10da11ad
Dulce Figueredo <dulcefigueredo2252@gmail.com>
2024-05-04 21:41:10 -0400

    The basic structure was created

---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="weather">
+      <h1 className="">Weather app</h1>
+    </div>
+  );
+}
